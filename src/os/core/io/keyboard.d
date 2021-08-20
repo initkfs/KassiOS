acdb42fd4897c3aa900f16d29237d051584d7d17
initkfs <none@none>
2021-08-21 00:12:52 +0300

    Add keyboard

---
+++ src/os/core/io/keyboard.d
@@ -0,0 +1,157 @@
+/**
+ * Authors: initkfs
+ */
+//https://wiki.osdev.org/PS/2_Keyboard
+module os.core.io.keyboard;
+
+private
+{
+	alias Ports = os.core.io.ports;
+}
+
+private __gshared char[178] scanCodeTable = [
+	'\?', '\?', //0 unused
+	'\?', '\?', //ESC
+	'1', '!', '2', '@', '3', '#', '4', '$', '5', '%', '6', '^', '7', '&', '8',
+	'*', '9', '(', '0', ')', '-', '_', '=', '+', '\b', '\b', //backspace
+	'\t', '\t', //tab
+	'q', 'Q', 'w', 'W', 'e', 'E', 'r', 'R', 't', 'T', 'y', 'Y', 'u', 'U', 'i',
+	'I', 'o', 'O', 'p', 'P', '[', '{', ']', '}', '\n', '\n', '\?', '\?', //left ctrl
+	'a',
+	'A', 's', 'S', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H', 'j', 'J', 'k', 'K',
+	'l', 'L', ';', ':', '\'', '\"', '`', '~', '\?', '\?', //left shift
+	'\\', '|', 'z', 'Z',
+	'x', 'X', 'c', 'C', 'v', 'V', 'b', 'B', 'n', 'N', 'm', 'M', ',', '<',
+	'.', '>', '/', '\?', '\?', '\?', //right shift
+	'\?', '\?', //keypad * or */PrtScrn
+	'\?', '\?', //left alt
+	' ', ' ', //space bar
+	'\?', '\?', //isCapsLockPress lock
+	'\?', '\?', //F1
+	'\?', '\?', //F2
+	'\?', '\?', //F3
+	'\?', '\?', //F4
+	'\?', '\?', //F5
+	'\?', '\?', //F6
+	'\?', '\?', //F7
+	'\?', '\?', //F8
+	'\?', '\?', //F9
+	'\?', '\?', //F10
+	'\?', '\?', //NumLock
+	'\?', '\?', //ScrollLock
+	'7', '\?', //Keypad-7/Home
+	'8', '\?', //Keypad-8/Up
+	'9', '\?', //Keypad-9/PgUp
+	'-', '\?', //Keypad -
+	'4', '\?', //Keypad-4/left
+	'5', '\?', //Keypad-5
+	'6', '\?', //Keypad-6/Right
+	'+', '\?', //Keypad +
+	'1', '\?', //Keypad-1/End
+	'2', '\?', //Keypad-2/Down
+	'3', '\?', //Keypad-3/PgDn
+	'4', '\?', //Keypad-0/Insert
+	'.', '\?', //Keypad ./Del
+	'\?', '\?', //Alt-SysRq
+	'\?', '\?', //F11 or F12. Depends
+	'\?', '\?', //non-US
+	'\?', '\?', //F11
+	'\?',
+	'\?' //F12
+];
+
+public enum ScanCodes
+{
+	CAPSLOCK = 0x3a,
+	LSHIFT = 0x2a,
+	RSHIFT = 0x36,
+	ESC = 0x01
+}
+
+private
+{
+	__gshared bool isShiftPress = false;
+	__gshared bool isCapsLockPress = false;
+}
+
+bool isReleased(const char code) @safe pure
+{
+	return (code & 128) == 128;
+}
+
+bool isPressed(const char code) @safe pure
+{
+	return !isReleased(code);
+}
+
+bool isSpecial(immutable(ubyte) code) @safe pure
+{
+	//TODO other codes
+	if (code == ScanCodes.LSHIFT || code == ScanCodes.RSHIFT || code == ScanCodes.CAPSLOCK)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+ubyte scanKeyCode()
+{
+	immutable ubyte scanCode = Ports.inport!(ubyte)(0x60);
+
+	if (isReleased(scanCode))
+	{
+
+		immutable ubyte releasedCode = cast(ubyte)(scanCode - 128);
+		switch (releasedCode)
+		{
+		case ScanCodes.CAPSLOCK:
+			break;
+		case ScanCodes.LSHIFT:
+			isShiftPress = false;
+			break;
+		case ScanCodes.RSHIFT:
+			isShiftPress = false;
+			break;
+		default:
+		}
+	}
+	else
+	{
+		switch (scanCode)
+		{
+		case ScanCodes.LSHIFT:
+			isShiftPress = true;
+			break;
+		case ScanCodes.RSHIFT:
+			isShiftPress = true;
+			break;
+		case ScanCodes.CAPSLOCK:
+			isCapsLockPress = !isCapsLockPress;
+			break;
+		default:
+		}
+	}
+
+	return scanCode;
+}
+
+char getKeyByCode(immutable(ubyte) scanCode)
+{
+	if (scanCode == 0 || isSpecial(scanCode) || isReleased(scanCode))
+	{
+		return 0;
+	}
+
+	immutable int charIndex = ((isShiftPress || isCapsLockPress) ? (scanCode * 2) + 1 : (
+			scanCode * 2));
+	immutable(char) resultChar = scanCodeTable[charIndex];
+	return resultChar;
+}
+
+char scanKey()
+{
+	immutable(ubyte) scanCode = scanKeyCode;
+	immutable(char) resultChar = getKeyByCode(scanCode);
+	return resultChar;
+}
