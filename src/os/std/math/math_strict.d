5775788ce58b5ab8b2cdf0062a791c603769d061
initkfs <none@none>
2021-08-25 13:56:34 +0300

    Improvements in strict math

--- src/os/std/math/math_strict.d
+++ src/os/std/math/math_strict.d
@@ -6,42 +6,75 @@ module os.std.math.math_strict;
 import std.traits;
 import os.std.errors;
 
-err addExact(T)(T a, T b, ref T sum) if (is(T == int) || is(T == long))
+// Warning! The checks are very simple and contain vulnerabilities
+
+err addExact(T)(T a, T b, ref T sum) if (isIntegral!T)
 {
     const T mustBeSum = a + b;
 
-    if ((a ^ b) >= 0 && (mustBeSum ^ b) < 0)
+    static if (isUnsigned!T)
     {
-        return error("Overflow occurred during addition");
+        if (mustBeSum < a)
+        {
+            return error("Overflow occurred during unsigned addition");
+        }
+        else
+        {
+            sum = mustBeSum;
+            return null;
+        }
     }
+    else
+    {
+        if ((a ^ b) >= 0 && (mustBeSum ^ b) < 0)
+        {
+            return error("Overflow occurred during signed addition");
+        }
 
-    sum = mustBeSum;
-    return null;
+        sum = mustBeSum;
+        return null;
+    }
 }
 
 unittest
 {
     import os.std.asserts : kassert;
 
-    long sum1;
-    kassert(addExact(long.max - 1, 1, sum1) is null);
-    kassert(sum1 == long.max);
+    uint sum1;
+    kassert(addExact(uint.max, 1u, sum1) !is null);
 
-    long sumOverflow;
-    kassert(addExact(long.max, 1, sumOverflow) !is null);
+    int sum2;
+    kassert(addExact(int.max, 1, sum2) !is null);
+
+    long sum3;
+    kassert(addExact(long.max - 1, 1, sum3) is null);
+    kassert(sum3 == long.max);
+
+    long sum4;
+    kassert(addExact(long.max, 1, sum4) !is null);
+
+    ulong sum5;
+    kassert(addExact(ulong.max, 1u, sum5) !is null);
 }
 
-err subtractExact(T)(T a, T b, ref T sub) if (is(T == int) || is(T == long))
+err subtractExact(T)(T a, T b, ref T sub) if (isIntegral!T)
 {
-    const T mustBeSub = a - b;
-
-    if ((a ^ b) < 0 && (mustBeSub ^ b) >= 0)
+    static if (isUnsigned!T)
     {
-        return error("Overflow occurred during subtraction");
+        return a < b ? error("Overflow occurred during unsigned subtraction") : (a - b);
+    }
+    else
+    {
+        const T mustBeSub = a - b;
+        if ((a ^ b) < 0 && (mustBeSub ^ b) >= 0)
+        {
+            return error("Overflow occurred during signed subtraction");
+        }
+
+        sub = mustBeSub;
+        return null;
     }
 
-    sub = mustBeSub;
-    return null;
 }
 
 unittest
@@ -52,6 +85,7 @@ unittest
     kassert(subtractExact(long.min, long.min, sub1) is null);
 }
 
+//TODO unsigned
 err multiplyExact(T)(T a, T b, T result) if (is(T == int) || is(T == long))
 {
     if (((b > 0) && (a > T.max / b || a < T.min / b)) || ((b < -1)
@@ -71,13 +105,12 @@ unittest
     kassert(multiplyExact(long.max, long.max, mul1) !is null);
 }
 
-err castExact(T, C)(T n, ref C result)
-        if ((is(T == long) && is(C == int)) || (is(T == int) && is(C == short))
-            || (is(T == short) && is(C == byte)))
+err castExact(T, C)(T n, ref C result) if (isIntegral!T && isIntegral!C)
 {
+    //TODO floating point
     if (n < C.min || n > C.max)
     {
-        return error("Unable to cast long to int: the number does not fit long size");
+        return error("Unable to cast exact: the source number does not fit target size");
     }
     result = cast(C) n;
     return null;
