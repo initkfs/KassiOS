9fb8f2bf1981022525d11a3c72ef48f0c1a50651
initkfs <none@none>
2021-12-01 19:40:48 +0300

    Changing Zig sources file structure

--- srcz/osz.zig
+++ srcz/osz.zig
@@ -1,5 +1,5 @@
 //pub use @import("./hash/hash.zig");
-const hash = @import("./text/hash.zig");
+const hash = @import("./stdos/text/hash.zig");
 
 export fn jenkins(data: [*:0]const u8) callconv(.C) u32 {
     return hash.jenkins(data);
