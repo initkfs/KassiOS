f02c4b0e7fa87f8bd13dec098ef95c9af1cb6e12
initkfs <none@none>
2021-12-16 21:32:36 +0300

    Fixes, add Simpson's rule for definite integrals

--- srcz/stdos/math/integration.zig
+++ srcz/stdos/math/integration.zig
@@ -7,7 +7,7 @@ fn f(x: f64) f64 {
     return 1 / (1 + x * x);
 }
 
-fn trapezoidal(a: f64, b: f64, n: i64) f64 {
+fn trapezoidal(a: f64, b: f64, n: u32) f64 {
     var h: f64 = (b - a) / @intToFloat(f64, n);
     var sum: f64 = f(a) + f(b);
     var i: usize = 1;
@@ -19,10 +19,46 @@ fn trapezoidal(a: f64, b: f64, n: i64) f64 {
 }
 
 test "Test Trapezoidal Rule for integrals" {
-    const a: f64 = 0;
-    const b: f64 = 1;
-
     const result: f64 = trapezoidal(0, 1, 6);
-    print("{d}\n", .{result});
+    //print("{d}\n", .{result});
     try testing.expect(result == 0.7842407666178157);
+}
+
+fn simpson(lower: f64, upper: f64, n: u32) f64 {
+    
+    if(n % 2 != 0){
+        return 0;
+    }
+    
+    var h: f64 = (upper - lower) / @intToFloat(f64, n);
+
+    var x = [_]f64{0} ** 10;
+    var fx = [_]f64{0} ** 10;
+
+    var i: usize = 0;
+    while (i <= n) : (i += 1) {
+        x[i] = lower + @intToFloat(f64, i) * h;
+        fx[i] = f(x[i]);
+    }
+
+    var res: f64 = 0;
+    var j: usize = 0;
+    while (j <= n) : (j += 1) {
+        if (j == 0 or j == n) {
+            res += fx[j];
+        } else if (j % 2 != 0) {
+            res += 4 * fx[j];
+        } else {
+            res += 2 * fx[j];
+        }
+    }
+
+    res = res * (h / 3);
+    return res;
+}
+
+test "Test Simpson's rule" {
+    const result: f64 = simpson(0, 1, 6);
+    //print("{d}\n", .{result});
+    try testing.expect(result == 0.7853979452340107);
 }
